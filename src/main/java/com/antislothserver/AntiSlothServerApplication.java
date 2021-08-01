54282776b6bcad2d4f29e0bd2cee2ee763090971
viiiraj07 <avisec07@gmail.com>
2021-08-01 11:22:56 +0530

    initial spring boot files

---
+++ src/main/java/com/antislothserver/AntiSlothServerApplication.java
@@ -0,0 +1,13 @@
+package com.antislothserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiSlothServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntiSlothServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antislothserver/AntiSlothServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antislothserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiSlothServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
