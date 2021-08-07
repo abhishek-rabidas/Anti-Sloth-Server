615b39c7a0e6ecaf837e4b16e509a41cf9595eb1
viiiraj07 <avisec07@gmail.com>
2021-08-07 21:40:54 +0530

    json ignore password

--- src/main/java/com/antislothserver/Models/User.java
+++ src/main/java/com/antislothserver/Models/User.java
@@ -1,5 +1,6 @@
 package com.antislothserver.Models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
@@ -10,6 +11,7 @@ import java.util.List;
 @Getter
 @Setter
 @Document(collection = "Users")
+@JsonIgnoreProperties(value = {"password"})
 public class User {
     @Id
     private String uid;
