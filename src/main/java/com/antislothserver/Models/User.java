2307425c2cee0247188f7c73971eb4302ff272f7
viiiraj07 <avisec07@gmail.com>
2021-08-07 22:25:02 +0530

    Revert "json ignore password"
    This reverts commit 615b39c7a0e6ecaf837e4b16e509a41cf9595eb1.

--- src/main/java/com/antislothserver/Models/User.java
+++ src/main/java/com/antislothserver/Models/User.java
@@ -1,6 +1,5 @@
 package com.antislothserver.Models;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
@@ -11,7 +10,6 @@ import java.util.List;
 @Getter
 @Setter
 @Document(collection = "Users")
-@JsonIgnoreProperties(value = {"password"})
 public class User {
     @Id
     private String uid;
